321fd55aa85b1852b636dbba46d7cf93e52312e8
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-01 21:11:06 +0200

    Spring Boot: Qualifiers

--- qualifiers/src/main/java/com/qualifiers/qualifiers/QualifiersApplication.java
+++ qualifiers/src/main/java/com/qualifiers/qualifiers/QualifiersApplication.java
@@ -1,13 +1,21 @@
 package com.qualifiers.qualifiers;
 
+import config.AppConfig;
+import demo.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class QualifiersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QualifiersApplication.class, args);
+
+		try(var context = new AnnotationConfigApplicationContext(AppConfig.class)){
+			Person person = context.getBean(Person.class);
+			person.sayHello("Bill");
+		}
 	}
 
 }
